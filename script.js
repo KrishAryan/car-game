279e87587f069f885f88b138e7ab1df7c3526f65
KrishAryan <krisharyan.36@gmail.com>
2021-11-19 20:54:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const score= document.querySelector(".score");
+const startScreen= document.querySelector(".startScreen");
+const gameArea= document.querySelector(".gameArea");
+startScreen.addEventListener('click',start)
+function start(){
+    console.log("click")
+}
